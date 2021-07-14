aa2383f2a2db72ddf4c1b6352828ad06268e22a3
Tor Morten Finnesand <tmf@kvstech.no>
2021-07-14 12:20:02 +0000

    fix dependency on pulseaudio

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,1 +1,6 @@
 # List of dependencies required for the project
+
+find_library(pulseaudio_LIBRARY pulse REQUIRED)
+find_library(pulseaudio_simple_LIBRARY pulse-simple REQUIRED)
+
+set(pulseaudio_LIBRARIES ${pulseaudio_LIBRARY} ${pulseaudio_simple_LIBRARY})
